dfd6fff56a95d342efe5949b8eb9b10bd77bec35
morfidon <armiksos@gmail.com>
2024-08-03 13:13:53 +0200

    New animations added to AnimatedSprited2D are now added to AnimationPlayer as new and old are kept

--- addons/animation_copier/animation_copier.gd
+++ addons/animation_copier/animation_copier.gd
@@ -1,71 +1,90 @@
 @tool
-extends EditorPlugin
+extends Node
 
-func copy_animations():
-	var edited_scene_root = get_editor_interface().get_edited_scene_root()
-	if not edited_scene_root:
-		printerr("No scene is currently being edited.")
-		return
+var editor_interface: EditorInterface
 
-	var animated_sprite = find_node_by_type(edited_scene_root, "AnimatedSprite2D")
-	var animation_player = find_node_by_type(edited_scene_root, "AnimationPlayer")
-
-	if not animated_sprite:
-		printerr("AnimatedSprite2D not found in the scene.")
-		return
+func copy_animations(animated_sprite: AnimatedSprite2D):
+	var animation_player = _find_or_create_animation_player(animated_sprite)
+	
 	if not animation_player:
-		printerr("AnimationPlayer not found in the scene.")
+		printerr("Failed to find or create AnimationPlayer.")
 		return
 
-	print("AnimatedSprite2D found: ", animated_sprite.name)
-	print("AnimationPlayer found: ", animation_player.name)
+	var animation_library = animation_player.get_animation_library("SpriteAnimations")
+	if not animation_library:
+		animation_library = AnimationLibrary.new()
+		animation_player.add_animation_library("SpriteAnimations", animation_library)
 
-	var animation_library = AnimationLibrary.new()
 	var animation_names = animated_sprite.sprite_frames.get_animation_names()
 	print("Animations found in AnimatedSprite2D: ", animation_names)
 
 	for anim_name in animation_names:
+		if animation_library.has_animation(anim_name):
+			print("Animation already exists: ", anim_name)
+			continue
+
 		print("Processing animation: ", anim_name)
 		var animation = Animation.new()
 		var frame_count = animated_sprite.sprite_frames.get_frame_count(anim_name)
 		var fps = animated_sprite.sprite_frames.get_animation_speed(anim_name)
 		print("Frame count: ", frame_count, ", FPS: ", fps)
 
-		animation.length = frame_count / float(fps)
+		var animation_duration = frame_count / float(fps)
+		animation.length = animation_duration
+
 		var track_index = animation.add_track(Animation.TYPE_VALUE)
-		
 		var path_to_sprite = animation_player.get_node(animated_sprite.get_path()).get_path()
-		var property_path = ":animation"
-		var full_path = str(path_to_sprite) + property_path
-		animation.track_set_path(track_index, NodePath(full_path))
-		
-		# Set the animation name
+		animation.track_set_path(track_index, NodePath(str(path_to_sprite) + ":animation"))
 		animation.track_insert_key(track_index, 0, anim_name)
 
-		# Add frame track
 		var frame_track_index = animation.add_track(Animation.TYPE_VALUE)
-		animation.track_set_path(frame_track_index, str(path_to_sprite) + ":frame")
+		animation.track_set_path(frame_track_index, NodePath(str(path_to_sprite) + ":frame"))
 
 		for frame in range(frame_count):
 			var time = frame / float(fps)
 			animation.track_insert_key(frame_track_index, time, frame)
+		
+		# Ensure the last frame extends to the end of the animation
+		animation.track_insert_key(frame_track_index, animation_duration, frame_count - 1)
 
 		animation.loop_mode = Animation.LOOP_LINEAR if animated_sprite.sprite_frames.get_animation_loop(anim_name) else Animation.LOOP_NONE
 
 		animation_library.add_animation(anim_name, animation)
 		print("Added animation to library: ", anim_name)
 
-	# Remove existing SpriteAnimations library if it exists
-	if animation_player.has_animation_library("SpriteAnimations"):
-		animation_player.remove_animation_library("SpriteAnimations")
-
-	animation_player.add_animation_library("SpriteAnimations", animation_library)
-
 	print("Animations copied successfully!")
-	print_animations(animation_player)
+	_print_animations(animation_player)
 
-	# Mark the scene as changed
-	get_editor_interface().get_resource_filesystem().scan()
+	editor_interface.get_resource_filesystem().scan()
+
+func _find_or_create_animation_player(animated_sprite: AnimatedSprite2D) -> AnimationPlayer:
+	var animation_player = _find_animation_player(animated_sprite)
+	
+	if not animation_player:
+		animation_player = AnimationPlayer.new()
+		animated_sprite.add_child(animation_player)
+		animation_player.owner = editor_interface.get_edited_scene_root()
+		print("Created new AnimationPlayer")
+	
+	return animation_player
+
+func _find_animation_player(node: Node) -> AnimationPlayer:
+	if node is AnimationPlayer:
+		return node
+	for child in node.get_children():
+		var found = _find_animation_player(child)
+		if found:
+			return found
+	return null
+
+func _print_animations(animation_player: AnimationPlayer):
+	print("Animation libraries in AnimationPlayer:")
+	var libraries = animation_player.get_animation_library_list()
+	for lib in libraries:
+		print("Library: ", lib)
+		var animations = animation_player.get_animation_library(lib).get_animation_list()
+		for anim in animations:
+			print("- ", anim)
 
 func find_node_by_type(node: Node, type_name: String) -> Node:
 	if node.get_class() == type_name:
@@ -75,12 +94,3 @@ func find_node_by_type(node: Node, type_name: String) -> Node:
 		if found:
 			return found
 	return null
-
-func print_animations(animation_player: AnimationPlayer):
-	print("Animation libraries in AnimationPlayer:")
-	var libraries = animation_player.get_animation_library_list()
-	for lib in libraries:
-		print("Library: ", lib)
-		var animations = animation_player.get_animation_library(lib).get_animation_list()
-		for anim in animations:
-			print("- ", anim)
